9714445f53c23fdee40fc6df7d18d20c9da38b4c
Spyros Panagiotopoulos <martkonsta@gmail.com>
2016-05-20 14:20:10 +0300

    Redirect to home page if url provided is empty

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ def index():
 @app.route('/new', methods=["POST"])
 def shortenLink():
 	ShortID, PrivKey = shortLink(request)
+	if ShortID == 'Invalid URL' or PrivKey == 400:
+		return redirect("/")
 	FullURI = getFullURL(ShortID, request)
 	if(request.headers.get("Accept") and request.headers.get("Accept") == "application/json"):
 		return Response('{\n\t"success":"true",\n\t"id":"' + ShortID + '",\n\t"link":"' + FullURI + '",\n\t"private_key": "' + PrivKey + '"\n}', mimetype="text/json")
